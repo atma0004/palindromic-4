9933a24d92024926c9a2da28349a2db81dd4fe72
atma0004 <atma0004@edu.eal.dk>
2017-02-02 22:28:57 +0100

    new

--- palindromic/Program.cs
+++ palindromic/Program.cs
@@ -4,34 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace palindromic
+namespace Project_Euler_problem
     {
     class Program
         {
         static void Main(string[] args)
             {
-            int sum;
-            int Largest = -1;
-            string res;
-            bool Palindrome = true;
-
-            for (int x = 999; x>=1; x--)
+            List<int> multipls = new List<int>();
+            for(int i = 1; i<1000; i++)
                 {
-                for (int y = 999; y>=1; y--)
+                if(i%3==0 ||i% 5==0)
                     {
-                    sum=x*y;
-                    res=sum.ToString();
-                    Palindrome=true;
-                    for (int t = 0; t<res.Length/2; t++)
-                        {
-                        if (res[t]!=res[res.Length-t-1]) Palindrome=false;
-                        }
-                    if (Palindrome) if (sum>Largest) Largest=sum;
+                    multipls.Add(i);
                     }
+                Console.WriteLine(multipls.Sum());
                 }
-            Console.WriteLine(Largest);
-            Console.ReadLine();
-
             }
         }
     }
